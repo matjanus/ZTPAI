2fef547784babed49cb00768d1dab8a7efccb825
Merekur <matti2000@onet.pl>
2025-05-28 21:17:37 +0200

    feat: backend add endpoint to delete yours account

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -2,8 +2,11 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use App\Service\UserService;
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
@@ -44,6 +47,19 @@ class UserController extends AbstractController
             return $this->json(['username' => $username]);
         } catch (\InvalidArgumentException $e) {
             return $this->json(['error' => $e->getMessage()], 404);
+        }
+    }
+
+    #[Route('/api/user/delete', name: 'user_delete', methods: ['DELETE'])]
+    public function deleteAccount(
+        #[CurrentUser] User $user,
+        UserService $userService
+    ): JsonResponse {
+        try {
+            $userService->deleteUser($user);
+            return new JsonResponse(null, 204);
+        } catch (\Throwable $e) {
+            return $this->json(['error' => $e->getMessage()], 400);
         }
     }
 
--- backend/src/Service/UserService.php
+++ backend/src/Service/UserService.php
@@ -12,18 +12,28 @@ class UserService
 
     private UserRepository $userRepository;
 
-    public function __construct(UserRepository $userRepository) {
+    public function __construct(
+        UserRepository $userRepository,
+        private EntityManagerInterface $em,
+        ) 
+    {
         $this->userRepository = $userRepository;
     }
 
-public function getUsername(int $id): string
-{
-    $username = $this->userRepository->findUsernameById($id);
+    public function getUsername(int $id): string
+    {
+        $username = $this->userRepository->findUsernameById($id);
 
-    if (!$username) {
-        throw new \InvalidArgumentException('The user does not exist.');
+        if (!$username) {
+            throw new \InvalidArgumentException('The user does not exist.');
+        }
+
+        return $username;
     }
 
-    return $username;
-}
+    public function deleteUser(User $user): void
+    {
+        $this->em->remove($user);
+        $this->em->flush();
+    }
 }
