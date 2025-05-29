557669a996a9abe90ce164814bc2a29cf6266447
Merekur <matti2000@onet.pl>
2025-05-29 19:27:13 +0200

    chore: frontend delete unused components userinfo

--- frontend/src/components/UserInfo.jsx
+++
@@ -1,9 +0,0 @@
-import './UserInfo.css';
-
-export default function UserInfo({ username }) {
-  return (
-    <div className="user-info">
-      <h2 className="user-info-title">Profil: {username}</h2>
-    </div>
-  );
-}
