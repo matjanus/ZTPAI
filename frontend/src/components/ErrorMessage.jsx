49c5c33dc615151faabd4d456432cf58b4b760df
Merekur <matti2000@onet.pl>
2025-05-29 08:57:36 +0200

    refactor: frontend errorpage add possibility to change code

--- frontend/src/components/ErrorMessage.jsx
+++ frontend/src/components/ErrorMessage.jsx
@@ -1,9 +1,9 @@
 import './ErrorMessage.css';
 
-export default function ErrorMessage({ message }) {
+export default function ErrorMessage({ code=404 , message }) {
   return (
     <div className="error-message">
-      <h1>Error 404</h1>
+      <h1>Error {code}</h1>
       <p>{message}</p>
     </div>
   );
